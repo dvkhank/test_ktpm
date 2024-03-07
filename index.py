3ac9a7c30d32891ed42c62235e3d339e467535b7
Khanhh <duongvankhanh.work@gmail.com>
2024-03-07 15:41:50 +0700

    20_DuongVanKhanh Cap nhat mot so bug

--- index.py
+++ index.py
@@ -84,14 +84,7 @@ def buy():
     })
 
 
-@app.route('/api/reply', methods=['post'])
-def rend_reply():
-    review_id = request.args.get('review')
-    product_id = request.args.get('product_id')
-    content = request.form.get('content')
-    dao.create_reply(review_id, content)
 
-    return redirect(url_for('product_detail', product_id=product_id))
 
 
 @app.route('/')
